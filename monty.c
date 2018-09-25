357d28acf6cdf219901cef18851b335659301f66
JackWanaCode <kazucuong@yahoo.com>
2018-09-25 23:31:18 +0000

    get the content in file

--- monty.c
+++ monty.c
@@ -1,5 +1,16 @@
 #include "monty.h"
 
+
+void push(int num)
+{
+	printf("in func num is %i\n", num);
+}
+
+void pall()
+{
+	printf("in func print all\n");
+}
+
 /**
  * main - Entry point
  * Description: run the m file
@@ -7,18 +18,44 @@
  */
 int main(int argc, char **argv)
 {
-
-	FILE *file = fopen(argv[1], "r");
 	char line[80];
-	if (!file)
+        char *token;
+        char array[2][80];
+        int i = 0, line_num = 1;
+
+	if (argc < 2)
 	{
 		perror("USAGE: monty file");
+                exit(EXIT_FAILURE);
+	}
+	FILE *file = fopen(argv[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 
 	while (fgets(line, sizeof(line), file))
 	{
-		printf("%s\n", line);
+		i = 0;
+		token = strtok(line, " \n");
+		while(token != NULL)
+		{
+			strcpy(array[i], token);
+			token = strtok(NULL, " \n");
+			i++;
+		}
+		if (strcmp(array[0], "push") == 0)
+			push(atoi(array[1]));
+		else if (strcmp(array[0], "pall") == 0)
+			pall();
+		else
+		{
+			fprintf(stderr, "L%i unknown instruction %s\n",\
+				line_num, array[0]);
+			exit(EXIT_FAILURE);
+		}
+		line_num++;
 	}
 	return (0);
 }
