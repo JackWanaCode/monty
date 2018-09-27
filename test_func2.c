448f2b3c38626795fec8c8b787a283017289afb3
anovacap <anovacap@yahoo.com>
2018-09-27 17:54:02 +0000

    add test_func2 to branch

---
+++ test_func2.c
@@ -0,0 +1,38 @@
+#include "monty.h"
+
+/**
+ * add - Adds the top two elements of the stack
+ * @stack: Struct stack_s as stack_t **ptr
+ * @line_number: File line number
+ * Return: Nothing (void)
+ */
+global_var global_variable;
+void add(stack_t **stack, unsigned int line_number)
+{
+	int j;
+	stack_t *runner = (*stack)->next;
+
+	if ((*stack)->next == NULL || (*stack) == NULL || stack == NULL)
+	{
+		fprintf(stderr, "%u: can't add, stack too short", line_number);
+		exit(EXIT_FAILURE);
+	}
+	j = (*stack)->n + runner->n;
+	runner->n = j;
+	runner->prev = NULL;
+	free(*stack);
+	*stack = runner;
+}
+
+/**
+ * nop - Does nothing
+ * @stack: Struct stack_s as stack_t **ptr
+ * @line_number: File line number
+ * Return: Nothing (void)
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) (*stack);
+	(void) line_number;
+	return;
+}
