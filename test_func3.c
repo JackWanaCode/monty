6d311d05a4c09a39f3469fe23ec08c0439684bee
JackWanaCode <kazucuong@yahoo.com>
2018-09-28 01:25:36 +0000

    correct mod and pchar

--- test_func3.c
+++ test_func3.c
@@ -15,7 +15,7 @@ void mod(stack_t **stack, unsigned int line_number)
 
 	if (stack == NULL || (*stack) == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
 		free_list(global_variable.stack);
 		fclose(global_variable.file);
 		exit(EXIT_FAILURE);
@@ -50,7 +50,7 @@ void pchar(stack_t **stack, unsigned int line_number)
 		fclose(global_variable.file);
 		exit(EXIT_FAILURE);
 	}
-	if ((*stack)->n < 0 || (*stack)->n > 177)
+	if ((*stack)->n < 0 || (*stack)->n > 127)
 	{
 		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
 		free_list(global_variable.stack);
